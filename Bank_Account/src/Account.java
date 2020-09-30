db33dd56d0fbe35e028e232138e252107279fedb
MagnoCarlos <68209004+MagnoCarlos@users.noreply.github.com>
2020-09-30 12:06:53 +0100

    Add files via upload

--- Bank_Account/src/Account.java
+++ Bank_Account/src/Account.java
@@ -1,45 +1,173 @@
+import com.mysql.cj.Query;
+import com.mysql.cj.protocol.Resultset;
+
+import java.sql.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 class Bank_Account {
 
     private int account_number;
     private int account_balance;
+    private int add_money;
+    private String account_name;
 
-    public Bank_Account(int account_number, int account_balance) {
+    public Bank_Account() {
 
-        this.account_number = account_number;
-        this.account_balance = account_balance;
+//        this.account_number = account_number;
+//        this.account_name = account_name;
 
-        System.out.println("Your account is: " + this.account_number);
+        // Save the details in the database
+
     }
 
-    public void deposit(int add_money) {
+    public void Add_Account(Connection conn, String account_name, int account_number, String account_Depo) {
 
-        if(add_money < 0) {
-            System.out.println("You cannot make a negative deposit");
-        } else {
-            this.account_balance = this.account_balance + add_money;
-            System.out.println("You added " + add_money + " to your account");
-            System.out.println("Your new balance is: " + this.account_balance);
+        try {
+
+            this.account_number = account_number;
+            this.account_name = account_name;
+            String query = "SELECT * FROM Bank_Account.Account_Data;";
+
+            // Save the details in the database
+            PreparedStatement stm = conn.prepareStatement("INSERT INTO Account_Data (Name, Account_Number) VALUES (?,?)");
+            stm.setString(1,account_name);
+            String account_numberString = Integer.toString(account_number);
+            stm.setString(2,account_numberString);
+            stm.execute();
+            //stm.executeUpdate();
+            //stm.close();
+
+            if(account_Depo == "y") {
+                System.out.println("How much do you want to deposit?");
+                Scanner input = new Scanner(System.in);
+                this.add_money = input.nextInt();
+                deposit(conn, add_money, account_number, account_name);
+            } else
+                // Add £0 to the account
+
+            System.out.println("Account Created");
+            System.out.println("Name: " + this.account_name);
+            System.out.println("Account number: " + this.account_number);
+
+        } catch (SQLException ex) {
+
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+
         }
+
     }
 
-    public void withdraw(int remove_money) {
+    public void deposit(Connection conn, double add_money, int account_number, String account_name) {
+        try {
 
-        if(remove_money < 0) {
-            System.out.println("You cannot make a negative withdraw");
-        } else if(remove_money > this.account_balance){
-            System.out.println("You cannot remove more money that your current balance");
-        } else {
-            this.account_balance = this.account_balance - remove_money;
-            System.out.println("You removed " + remove_money + " from your account");
-            System.out.println("Your new balance is: " + this.account_balance);
+            if(add_money < 0) {
+                System.out.println("You cannot make a negative withdraw");
+            }
+
+            // Pull out the data from the dataBase
+            PreparedStatement stm = conn.prepareStatement("SELECT Money\n" +
+                    "FROM `Bank_Account`.`Account_Data` where Name = ? and Account_Number = ?;");
+            stm.setString(1,account_name);
+            String add_moneyString = Integer.toString(account_number); // convert INT to STRING
+            stm.setString(2,add_moneyString);
+            ResultSet rs = stm.executeQuery();
+
+            String display_Money;
+            double money_Added = 0;
+            while (rs.next()) {
+
+                display_Money = rs.getString("Money"); //get the current money in the account
+                double money_AddedConversion = Double.parseDouble(display_Money);
+
+                money_Added = money_AddedConversion + add_money; // add to the current balance
+                System.out.println("You added: " + display_Money + " to your account");
+                System.out.println("Your balance is: " + money_Added);
+
+                // Update the new balance into the database
+            }
+
+        } catch (SQLException ex) {
+
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+
         }
-    }
 
-    public void balance(int account_number) {
+    } // deposit
 
-        this.account_balance = this.account_balance;
+    public void withdraw(Connection conn, String account_name, int account_number, double remove_money) {
+        try {
 
-        System.out.println("Your account balance is: " + this.account_number);
-    }
+            if(remove_money < 0) {
+                System.out.println("You cannot make a negative withdraw");
+            }
+
+            // Pull out the data from the dataBase
+            PreparedStatement stm = conn.prepareStatement("SELECT Money\n" +
+                    "FROM `Bank_Account`.`Account_Data` where Name = ? and Account_Number = ?;");
+            stm.setString(1,account_name);
+            String add_moneyString = Integer.toString(account_number); // convert INT to STRING
+            stm.setString(2,add_moneyString);
+            ResultSet rs = stm.executeQuery();
+
+            String display_Money;
+            double money_Removed = 0;
+            while (rs.next()) {
+
+                display_Money = rs.getString("Money"); //get the current money in the account
+                double money_RemovedConversion = Double.parseDouble(display_Money);
+                if(remove_money > money_RemovedConversion) {
+                    System.out.println("You cannot remove more money that your current balance");
+                } else {
+                    money_Removed = money_RemovedConversion - remove_money; // subtract from the current balance
+                    System.out.println("You removed: " + display_Money + " from your account");
+                    System.out.println("Your balance is: " + money_Removed);
+
+                    // Update the new balance into the database
+                }
+            }
+
+        } catch (SQLException ex) {
+
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+
+        }
+
+    } // withdraw
+
+    public void balance(Connection conn, String account_name, int account_number) {
+        try {
+
+            // Pulling the Money data from the DataBase
+            PreparedStatement stm = conn.prepareStatement("SELECT Money\n" +
+                    "FROM `Bank_Account`.`Account_Data` where Name = ? and Account_Number = ?;");
+
+            stm.setString(1,account_name);
+            String add_moneyString = Integer.toString(account_number);
+            stm.setString(2,add_moneyString);
+            ResultSet rs = stm.executeQuery();
+
+            String display_Money = null;
+            while (rs.next()) {
+
+                display_Money = rs.getString("Money");
+                System.out.println(display_Money);
+            }
+            System.out.println("Your balance is: " + display_Money);
+            //stm.executeUpdate();
+            //System.out.println("Your account balance is: " + this.account_number);
+
+        } catch (SQLException ex) {
+
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+
+        }
+
+    } // balance
 
 }
--- Bank_Account/src/Main.java
+++ Bank_Account/src/Main.java
@@ -1,51 +1,123 @@
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.awt.*;
 import java.lang.Math;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
 
+    Connection connect;
+    Statement stm;
     public static void main(String[] args) {
 
+        //Establishing connection
+        Main connection = new Main();
+        Connection connect_ = connection.Create_Connection();
+
+
+        Bank_Account new_account = new Bank_Account();
+        // Quick introduction to the program
         System.out.println("This is a simple Bank Account Model Design in Java.");
-        System.out.println("You will be able to make deposit, withdraw money and check balance.");
+        System.out.println("You will be able to add a new account, make deposit, withdraw money and check balance.");
         System.out.println("");
 
-        System.out.println("Add Account Number: ");
+        // Request for the user's account number and immediate deposit
+
+        System.out.println("Please follow the options below:");
+        System.out.println("1. Add a new account.");
+        System.out.println("2. Make a deposit.");
+        System.out.println("3. Check balance.");
+        System.out.println("4. Withdraw money.");
+
+        int inputAccount;
+        Double add_money;
         Scanner input = new Scanner(System.in);
-        int inputNum = input.nextInt();
-
-        System.out.println("Make a deposit: ");
-        input = new Scanner(System.in);
-        int inputDepo = input.nextInt();
-
-        Bank_Account new_account = new Bank_Account(inputNum,inputDepo);
-
-        System.out.println("Please follow the options below.");
-        System.out.println("1. Make a deposit.");
-        System.out.println("2. Check balance.");
-        System.out.println("3. Withdraw money.");
-
-        input = new Scanner(System.in);
         int options = input.nextInt();
-        if (options == 1) {
-            System.out.println("How much do you want to deposit?");
-            input = new Scanner(System.in);
-            inputDepo = input.nextInt();
-            new_account.deposit(inputDepo);
+        switch(options) {
+            case 1: // Get the details NEW ACCOUNT
+                    System.out.println("Name: ");
+                    input = new Scanner(System.in);
+                    String inputName = input.nextLine();
+                    System.out.println("Add Account Number: ");
+                    input = new Scanner(System.in);
+                    inputAccount = input.nextInt();
+                    System.out.println("Make a deposit: Y/N");
+                    input = new Scanner(System.in);
+                    String inputDepo = input.nextLine();
 
-        } else if (options == 2) {
-            new_account.balance(inputNum);
+                    // Call Function
+                    new_account.Add_Account(connect_, inputName, inputAccount, inputDepo);
 
-        } else if (options == 3) {
-            System.out.println("How much do you want to withdraw?");
-            input = new Scanner(System.in);
-            int inputWithdraw = input.nextInt();
-            new_account.withdraw(inputWithdraw);
-        } else
-            System.out.println("Invalid");
+                    System.out.println("Thanks!");
+                break;
 
+            case 2: // DEPOSIT
+                    System.out.println("Name: ");
+                    input = new Scanner(System.in);
+                    inputName = input.nextLine();
+                    System.out.println("Account Number: ");
+                    input = new Scanner(System.in);
+                    inputAccount = input.nextInt();
+                    System.out.println("How much do you want to deposit?");
+                    input = new Scanner(System.in);
+                    double inputDeposit = input.nextDouble();
+                    new_account.deposit( connect_, inputDeposit, inputAccount, inputName);
+                break;
 
-    }
+            case 3: // BALANCE
+                    System.out.println("Name: ");
+                    input = new Scanner(System.in);
+                    inputName = input.nextLine();
+                    System.out.println("Account Number: ");
+                    input = new Scanner(System.in);
+                    inputAccount = input.nextInt();
+                    new_account.balance( connect_, inputName, inputAccount);
+                break;
 
-}
+            case 4: // WITHDRAW
+                    System.out.println("Name: ");
+                    input = new Scanner(System.in);
+                    inputName = input.nextLine();
+                    System.out.println("Account Number: ");
+                    input = new Scanner(System.in);
+                    inputAccount = input.nextInt();
+                    System.out.println("How much do you want to withdraw?");
+                    input = new Scanner(System.in);
+                    double inputWithdraw = input.nextDouble();
+                    new_account.withdraw( connect_, inputName, inputAccount, inputWithdraw);
+                break;
+
+            default: System.out.println("Invalid");
+                break;
+
+        }
+
+    } // end main
+
+    // Create Connection with the database
+    public Connection Create_Connection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/Bank_Account", "root","S@b3d0ri@My5ql" );
+            stm = connect.createStatement();
+
+            if (connect != null)
+                System.out.println("Connected...");
+
+        } catch (ClassNotFoundException ex) {
+
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+
+        } catch (SQLException ex) {
+
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+
+        }
+
+        return connect;
+    } // Create_Connection
+
+}// end Main
